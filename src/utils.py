e6dc55da8a45c7f177539733dde7b6971476e535
Dmitry Savostyanov <dmsavostyanov123@gmail.com>
2022-08-04 15:14:36 +0300

    Find match_id by youtube header

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,9 @@
 import logging
-from typing import Any
+from typing import Any, List, Dict
+
+import requests
+from tenacity import retry, stop_after_attempt, wait_fixed
+from loguru import logger
 
 
 class DisableLogger:
@@ -13,3 +17,17 @@ class DisableLogger:
 class EmptyDebugLogger:
     def debug(self):
         pass
+
+
+@retry(stop=stop_after_attempt(3), wait=wait_fixed(2))
+def query_opendota(sql: str, **kwargs: dict) -> List[Dict]:
+    query = sql.format(**kwargs)
+    logger.debug(query)
+    query = query.replace('\n', ' ').replace('\t', ' ')
+    query = ' '.join(word for word in query.split(' ') if word)
+
+    r = requests.get('https://api.opendota.com/api/explorer', params=dict(sql=query))
+    r.raise_for_status()
+    result = r.json()
+    rows = result['rows']
+    return rows
