1f9f6ec33a792ab62b6d6c7a62bd2363a489ab9f
Dmitry Savostyanov <dmsavostyanov123@gmail.com>
2022-09-01 11:50:56 +0300

    highlight signals

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,8 @@
 import logging
-from typing import Any, List, Dict
+from typing import Any, List, Dict, Tuple
 
 import requests
+import pandas as pd
 from tenacity import retry, stop_after_attempt, wait_fixed
 from loguru import logger
 
@@ -34,3 +35,35 @@ def query_opendota(sql: str, **kwargs: dict) -> List[Dict]:
     result = r.json()
     rows = result['rows']
     return rows
+
+
+class TimeSeries(pd.Series):
+    def __init__(self, *args: Tuple, **kwargs: Dict):
+        super().__init__(*args, **kwargs)
+
+    def t(self, start_time: int = None, end_time: int = None) -> pd.Series:
+        if start_time is not None and end_time is not None:
+            series = self[(self.index >= start_time) & (self.index <= end_time)]
+        elif start_time is not None and end_time is None:
+            series = self[(self.index >= start_time)]
+        elif end_time is not None and start_time is None:
+            series = self[(self.index <= end_time)]
+        else:
+            series = self
+        return TimeSeries(series)
+
+
+class TimeTable(pd.DataFrame):
+    def __init__(self, *args: Tuple, **kwargs: Dict):
+        super().__init__(*args, **kwargs)
+
+    def t(self, start_time: int = None, end_time: int = None) -> pd.DataFrame:
+        if start_time is not None and end_time is not None:
+            df = self[(self['time'] >= start_time) & (self['time'] <= end_time)]
+        elif start_time is not None and end_time is None:
+            df = self[(self['time'] >= start_time)]
+        elif end_time is not None and start_time is None:
+            df = self[(self['time'] <= end_time)]
+        else:
+            df = self
+        return TimeTable(df)
