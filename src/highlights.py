e1e832bcf60251266279ae6eb836f6b0e5929dff
Dmitry Savostyanov <dmsavostyanov123@gmail.com>
2022-09-06 17:02:30 +0300

    as_target and as_attacker methods

--- src/highlights.py
+++ src/highlights.py
@@ -58,17 +58,13 @@ def merge_close_intervals(intervals: List[Dict], gap: int) -> List[Dict]:
 
 
 def has_intersection(interval1: Dict, interval2: Dict, gap: int = 0) -> bool:
-    """
-    By default gap = 0, checks for strict intersection
-    """
+    """By default gap = 0, checks for strict intersection"""
     merged = merge_close_intervals([interval1, interval2], gap)
     return len(merged) < 2
 
 
 def find_hp_decreasing_intervals(player: MatchPlayer) -> List[Dict]:
-    """
-    Signal with intervals where player has negative hp diff
-    """
+    """Signal with intervals where player has negative hp diff"""
     binary_mask = player.sdhp < HP_RATE_THRESHOLD
     intervals = convert_binary_mask_to_intervals(binary_mask)
     intervals = merge_close_intervals(intervals, MERGE_GAP)
@@ -76,9 +72,7 @@ def find_hp_decreasing_intervals(player: MatchPlayer) -> List[Dict]:
 
 
 def find_low_hp_intervals(player: MatchPlayer) -> List[Dict]:
-    """
-    Signal with intervals where player has low hp
-    """
+    """Signal with intervals where player has low hp"""
     series = player.hp / player.max_hp
     binary_mask = series < MAX_HP_THRESHOLD
     intervals = convert_binary_mask_to_intervals(binary_mask)
@@ -87,9 +81,7 @@ def find_low_hp_intervals(player: MatchPlayer) -> List[Dict]:
 
 
 def find_low_and_decreasing_hp_intervals(player: MatchPlayer) -> List[Dict]:
-    """
-    Signal with intervals where player has negative hp diff and has low hp
-    """
+    """Signal with intervals where player has negative hp diff and has low hp"""
     hp_decreasing_intervals = find_hp_decreasing_intervals(player)
     low_hp_intervals = find_low_hp_intervals(player)
     low_and_decreasing_hp_intervals = []
@@ -100,3 +92,24 @@ def find_low_and_decreasing_hp_intervals(player: MatchPlayer) -> List[Dict]:
             if has_intersection(hp_decreasing_interval, low_hp_interval):
                 low_and_decreasing_hp_intervals.append(hp_decreasing_interval)
     return low_and_decreasing_hp_intervals
+
+
+def find_attacks(player: MatchPlayer) -> List[Dict]:
+    """Intervals where player was attecked"""
+    match = player.match
+    intervals = find_low_and_decreasing_hp_intervals(player)
+    for interval in intervals:
+        is_dead = player.deaths.t(interval['start'], interval['end']).shape[0]
+        if is_dead:
+            interval['target_dead'] = True
+        else:
+            interval['target_dead'] = False
+
+        df_input_damage = player.hero_damage_in.t(interval['start'], interval['end'])
+        attacker_heroes = df_input_damage['sourcename'].unique()
+        converted_to_players = []
+        for hero_name in attacker_heroes:
+            hero_player = match.get_player(hero_name)
+            converted_to_players.append(hero_player)
+        interval['attacker_heroes'] = converted_to_players
+    return intervals
